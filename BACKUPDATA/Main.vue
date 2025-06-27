4c5bae18c13e6bf18b485ca62b1c7f32736fe9ed
DevOps VsCode <devops.benjamin@gmail.com>
2025-06-27 21:01:06 +0200

    Tailwind pb

---
+++ BACKUPDATA/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Vite HOTRELOAD</h1>
+  <h1>VR Hypno - Mode Éditeur</h1>
+  <button @click="goToPlayer">Basculer en mode Player</button>
+</template>
+
+<script setup lang="ts">
+import { useI18n, Locale } from '../shared/utils/i18n';
+import { ref } from 'vue';
+
+const isDev = import.meta.env && import.meta.env.DEV;
+
+const locale = ref<Locale>('fr'); // or 'en', can be dynamic
+const t = useI18n(locale);
+
+// Usage: t('appTitle').value
+
+async function goToPlayer() {
+  if (isDev) {
+    window.location.replace("http://localhost:5174");
+  } else {
+    window.location.replace("/player/index.html");
+  }
+}
+</script>
